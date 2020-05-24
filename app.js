524519a31c679fd852862c3ae9c1309642ba7b72
Alexander G. Yang <spark8402@rogers.com>
2020-05-24 16:57:34 -0400

    added creating form function

--- app.js
+++ app.js
@@ -1,37 +1,59 @@
+const courseCategoryInput = document.querySelector(".course-category-input")
+const courseCategoryForm = document.querySelector(".course-category-form")
+const courseCategoryList = document.querySelector(".course-category-list");
+const courseForm = document.querySelector(".course-form")
 const courseInput = document.querySelector(".course-input")
-const form = document.querySelector('form')
-const courseList = document.querySelector(".course-list");
+const courseCategory = document.querySelector(".course-category")
 
+courseCategoryForm.addEventListener("submit", addCourseCategory);
+courseForm.addEventListener("submit", addCourse);
 
-form.addEventListener("submit", addCourse);
-
-function addCourse(e){
+function addCourseCategory(e){
     e.preventDefault();
-    console.log(courseInput.value)
+    console.log(courseCategoryInput.value)
 
-    const courseDiv = document.createElement("div");
-    courseDiv.classList.add("course");
-    //Create list
-    const newCourse = document.createElement("li");
-    newCourse.innerText = courseInput.value;
-    //Save to local - do this last
+    const courseCategoryDiv = document.createElement("div");
+    courseCategoryDiv.classList.add("course-category");
+    const newCourseCategory = document.createElement("li");
+    newCourseCategory.innerText = courseCategoryInput.value;
     //Save to local
-    saveLocalCourses(courseInput.value);
-    newCourse.classList.add("course-item");
-    courseDiv.appendChild(newCourse);
-    courseInput.value = ""; 
-    // Create Completed Button
-    const completedButton = document.createElement("button");
-    completedButton.innerHTML = `<i class="fas fa-check"></i>`;
-    completedButton.classList.add("complete-btn");
-    courseDiv.appendChild(completedButton);
+    saveLocalCourses(courseCategoryInput.value);
+    newCourseCategory.classList.add("course-category-item");
+    courseCategoryDiv.appendChild(newCourseCategory);
+    courseCategoryInput.value = ""; 
+    // Create Button
+    // const completedButton = document.createElement("button");
+    // completedButton.innerHTML = `<i class="fas fa-plus"></i>`;
+    // completedButton.classList.add("complete-btn");
+    // courseCategoryDiv.appendChild(completedButton);
     //Create trash button
     const trashButton = document.createElement("button");
     trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
     trashButton.classList.add("trash-btn");
-    courseDiv.appendChild(trashButton);
-    //attach final Todo
-    courseList.appendChild(courseDiv);
+    courseCategoryDiv.appendChild(trashButton);
+    //Create Form
+    courseCategoryList.appendChild(courseCategoryDiv);
+    const addCourseForm = document.createElement("form");
+    addCourseForm.classList.add("course-form");
+    addCourseForm.innerHTML = `                        
+        <input type="text" class="course-input" />
+        <button class="course-button" type="submit">
+        <i class="fas fa-plus-square"></i>
+        </button>
+    `;
+    courseCategoryDiv.appendChild(addCourseForm);
+}
+
+function addCourse(e){
+    e.preventDefault();
+    console.log(event.currentTarget)
+    console.log(courseInput.value)
+    const courseDiv = document.createElement("div");
+    const courseList = document.createElement("li");
+    courseList.innerText = courseInput.value
+    courseDiv.appendChild(courseList);
+    courseCategory.appendChild(courseDiv);
+    courseInput.value = "";
 }
 
 function saveLocalCourses(course) {
